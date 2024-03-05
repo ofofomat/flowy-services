66d29fe0869f9a5af2481fe4748c94a5229589b5
MATEUS FERNANDES <mateus.fernandes@catolicasc.edu.br>
2024-03-05 20:24:30 -0300

    Template initilzr

---
+++ src/main/java/br/edu/catolicasc/flowyservices/FlowyServicesApplication.java
@@ -0,0 +1,13 @@
+package br.edu.catolicasc.flowyservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowyServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowyServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/catolicasc/flowyservices/FlowyServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.catolicasc.flowyservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowyServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
