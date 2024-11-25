65996c26c3274da6bb2fd8fd027c403ff1307e00
Fernandes <e-mateusf@weg.net>
2024-11-25 09:41:09 -0300

    FLY-0023 --> Corrige problema de variáveis explícitas.

--- src/main/java/br/edu/catolicasc/flowyservices/FlowyServicesApplication.java
+++ src/main/java/br/edu/catolicasc/flowyservices/FlowyServicesApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import br.edu.catolicasc.flowyservices.config.CorsConfig;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @Import(CorsConfig.class)
 @SpringBootApplication
 public class FlowyServicesApplication {
 
     public static void main(String[] args) {
+        Dotenv.load();
         SpringApplication.run(FlowyServicesApplication.class, args);
     }
 
